e3217f66d3c07cbb7215614d876a28ba912fe026
HSgod <hsgodkontakt@gmail.com>
2020-11-17 21:06:15 +0100

    Cleanup brightness bar customization
    Revert "Allow showing bottom brightness slider above QS footer [2/2]"
    
    This reverts commit 79dcaee7dbd3a4cf7d747e816f3e994a0168a01a.
    
    Revert "Allow hiding auto brightness toggle [2/2]"
    
    This reverts commit a23c97758c89079a8949dc8985e5cd6342df7755.
    
    Revert "Add pref for qs brightness slider position [2/2]"
    
    This reverts commit 7238a9c60232b0abdce0ab881f5aacd1ad31c13d.

--- src/com/streak/settings/fragments/Statusbar.java
+++ src/com/streak/settings/fragments/Statusbar.java
@@ -27,7 +27,6 @@ import android.view.View;
 import com.android.settings.SettingsPreferenceFragment;
 import com.streak.settings.preferences.CustomSeekBarPreference;
 import com.streak.settings.preferences.SystemSettingSwitchPreference;
-import com.streak.settings.preferences.SystemSettingMasterSwitchPreference;
 import com.android.settings.Utils;
 import android.util.Log;
 
@@ -42,9 +41,6 @@ public class Statusbar extends SettingsPreferenceFragment implements
 
     private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mNetMonitor;
-    private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
-
-    private SystemSettingMasterSwitchPreference mBrightnessSlider;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -54,13 +50,6 @@ public class Statusbar extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
         final ContentResolver resolver = getActivity().getContentResolver();
-
-        mBrightnessSlider = (SystemSettingMasterSwitchPreference)
-                findPreference(BRIGHTNESS_SLIDER);
-        mBrightnessSlider.setOnPreferenceChangeListener(this);
-        boolean enabled = Settings.System.getInt(resolver,
-                BRIGHTNESS_SLIDER, 1) == 1;
-        mBrightnessSlider.setChecked(enabled);
 
         boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT) == 1;
@@ -77,8 +66,8 @@ public class Statusbar extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final ContentResolver resolver = getActivity().getContentResolver();
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
         if (preference == mNetMonitor) {
             boolean value = (Boolean) newValue;
             Settings.System.putIntForUser(getActivity().getContentResolver(),
@@ -92,11 +81,6 @@ public class Statusbar extends SettingsPreferenceFragment implements
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
                     UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mBrightnessSlider) {
-            Boolean value = (Boolean) newValue;
-            Settings.System.putInt(resolver,
-                    BRIGHTNESS_SLIDER, value ? 1 : 0);
             return true;
         }
         return false;
